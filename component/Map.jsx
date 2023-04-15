f001554b73a0e7b51a1bab5b22199bfb1a2752f1
AeksP <alex.pascual.batet@estudiantat.upc.edu>
2023-04-15 19:29:29 +0200

    Updated Map Local/Global
    Mapa global & mapa local en la misma página.

--- component/Map.jsx
+++ component/Map.jsx
@@ -9,6 +9,7 @@ export default function Map({ mapType }) {
 
   useEffect(() => {
     async function fetchData() {
+      if (mapType === "global"){
       if (map) return;
       const mapboxMap = new mapboxgl.Map({
         container: 'mapbox-map',
@@ -28,6 +29,36 @@ export default function Map({ mapType }) {
 
       // Agrega la capa de línea para el recorrido
       mapboxMap.on('load', () => {
+        //Agregar almacen
+        mapboxMap.addLayer({
+          id: 'warehouse',
+          type: 'symbol',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [{
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [1.7474957, 41.2278786]
+                },
+                properties: {
+                  title: 'Almacén',
+                  icon: 'industry'
+                }
+              }]
+            }
+          },
+          layout: {
+            'icon-image': '{icon}',
+            'icon-size': 2,
+            'text-field': '{title}',
+            'text-offset': [0, 0.75],
+            'text-anchor': 'top'
+          }
+        });
+
         // Agrega la línea para el recorrido
         mapboxMap.addLayer({
         id: 'ruta',
@@ -67,7 +98,7 @@ export default function Map({ mapType }) {
                     },
                     properties: {
                       title: 'Les Roquetes del Garraf, Sant Pere de Ribes',
-                      icon: 'marker'
+                      icon: 'car'
                     }
                   },
                   {
@@ -85,22 +116,147 @@ export default function Map({ mapType }) {
               }
             },
             layout: {
-              'icon-image': '{icon}-15',
+              'icon-image': '{icon}',
+              'icon-size': 2,
               'text-field': '{title}',
-              'text-offset': [0, 1.5],
+              'text-offset': [0, 0.75],
+              'text-anchor': 'top'
+            }
+          });
+      });
+    }else if(mapType === "local"){
+      if (map) return;
+      const mapboxMap = new mapboxgl.Map({
+        container: 'mapbox-map',
+        style: 'mapbox://styles/aeksp/clg9out5b000i01l0p2yiq26g',
+        center: [1.7439067, 41.2274815], // centra el mapa en una ubicación específica
+        zoom: 15 // establece el nivel de zoom del mapa
+      });
+      setMap(mapboxMap);
+      mapboxMap.addControl(new mapboxgl.NavigationControl());
+
+      // Obtener la ruta desde la API Directions de Mapbox
+      const response = await fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${1.7439067},${41.2274815};${1.7651},${41.2583}?geometries=geojson&alternatives=true&access_token=${mapboxgl.accessToken}`
+      );
+      const data = await response.json();
+      const ruta = data.routes[0].geometry;
+
+      // Agrega la capa de línea para el recorrido
+      mapboxMap.on('load', () => {
+        //Agregar almacen
+        mapboxMap.addLayer({
+          id: 'warehouse',
+          type: 'symbol',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [{
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [1.7474957, 41.2278786]
+                },
+                properties: {
+                  title: 'Almacén',
+                  icon: 'industry'
+                }
+              }]
+            }
+          },
+          layout: {
+            'icon-image': '{icon}',
+            'icon-size': 2,
+            'text-field': '{title}',
+            'text-offset': [0, 0.75],
+            'text-anchor': 'top'
+          }
+        });
+
+        // Agrega la línea para el recorrido
+        mapboxMap.addLayer({
+        id: 'ruta',
+        type: 'line',
+        source: {
+            type: 'geojson',
+            data: {
+            type: 'Feature',
+            properties: {},
+            geometry: ruta
+            }
+        },
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+            'line-color': '#0080ff',
+            'line-width': 8
+        }
+        });
+
+        // Agrega la capa de símbolo para el inicio y el final del recorrido
+        mapboxMap.addLayer({
+            id: 'puntos-de-interes',
+            type: 'symbol',
+            source: {
+              type: 'geojson',
+              data: {
+                type: 'FeatureCollection',
+                features: [
+                  {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: [1.7439067, 41.2274815]
+                    },
+                    properties: {
+                      title: 'Les Roquetes del Garraf, Sant Pere de Ribes',
+                      icon: 'a',
+                    }
+                  },
+                  {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: [1.7651, 41.2583]
+                    },
+                    properties: {
+                      title: 'Tu destino',
+                      icon: 'marker'
+                    }
+                  }
+                ]
+              }
+            },
+            layout: {
+              'icon-image': '{icon}',
+              'icon-size': [
+                'case',
+                ['==', ['get', 'icon'], 'a'], // si el icono es 'a'
+                ['/', 1, 15], // tamaño original dividido por 15
+                ['==', ['get', 'icon'], 'marker'], // si el icono es 'marker'
+                2, // tamaño original multiplicado por 2
+                1 // tamaño default
+              ],
+              'text-field': '{title}',
+              'text-offset': [0, 0.75],
               'text-anchor': 'top'
             }
           });
       });
     }
 
+    }
+
     fetchData();
-  }, [map]);
+  }, [mapType, map]);
 
   return (
     <div className={mapStyles.mapContainer}>
       Mapa {mapType}
-      <div id="mapbox-map" style={{ width: "100%", height: "400px" }}></div>
+      <div id="mapbox-map" style={{ width: "100%", height: "250px" }}></div>
     </div>
   );
 }
