ed17a3dbbe11d45ff245b68511c1f0663e95286e
edu <eduard.cole@estudiantat.upc.edu>
2023-04-20 13:58:06 +0200

    actualització .json i altres

--- src/pages/makeorder.jsx
+++ src/pages/makeorder.jsx
@@ -1,7 +1,25 @@
 import Head from 'next/head'
 import Layout from "../component/Layout"
+import Table from "../component/Tabla_makeOrder"
 
-export default function Home() {
+export async function getStaticProps() {
+  //función de nextjs que se encarga de cargar los datos pedidos.json (placeholder que después se cambiará por la API)
+  const filePath = path.join('public/', 'medicamentos.json');
+  const jsonData = await fsPromises.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: data
+  }
+}
+
+export default function Home(props) {
+ //recibe props: pedidos
+ //searchValue es un estado que guarda el texto de busqueda del componente MyOrdersSearch
+ //se declara aqui arriba para pasarlo a los hijos, actualizar el State y pasarlo a TablaPedidos
+ const [searchValue, setSearchValue] = useState({value:"",isCompleted:false});
+ const medicamentos = props.medicamentos;
+
   return (
     <>
       <Head>
@@ -10,9 +28,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Layout>
-          todo
-        </Layout>
+        {/**le pasamos a Layout el valor del componente de la página que está renderizando (Layout se encarga de detectar en que pagina está) */}
+        <Layout navBarValue={setSearchValue}>
+        <div className={myordersStyles.mainContainer}>
+          {/**TablaPedidos recibe cuantas filas va a renderizar, los datos y el valor para filtrar en caso d eque haya */}
+          <Table data={medicamentos} rowsPerPage={10} searchValue={searchValue} setSearchValue={setSearchValue}/>
+        </div>
+        </Layout> 
       </main>
     </>
   )
