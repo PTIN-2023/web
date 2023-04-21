fd19c1812c8786c3a3973f3a272c2b1a4957f905
edu <eduard.cole@estudiantat.upc.edu>
2023-04-21 12:23:43 +0200

    actualització

--- pages/makeorder.jsx
+++ pages/makeorder.jsx
@@ -1,20 +1,34 @@
+import React, { Component } from 'react'
+import '../styles/MakeOrder.module.css'
 import Head from 'next/head'
 import Layout from "../component/Layout"
+import data from "../public/medicamentos.json"
+import Table from "../component/TablaMakeorder"
+import myordersStyles from "../styles/Myorders.module.css"
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>TransMedWebPTIN</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Layout>
-          todo
-        </Layout>
-      </main>
-    </>
-  )
+
+function Home() {
+	const getHeadings = () => {
+		return Object.keys(data[0]);
+	}
+
+	return (
+		<>
+			<Head>
+				<title>TransMedWebPTIN</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Layout>
+				<div className="Tabla">
+					<Table theadData={getHeadings()} tbodyData={data} />
+				</div>
+				</Layout>
+			</main>
+		</>
+	);
 }
+
+export default Home;
 
