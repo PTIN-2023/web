d553a45554fde4b298f8759923c9b34f87af905f
edu <eduard.cole@estudiantat.upc.edu>
2023-04-30 19:07:36 +0200

    makeOrder

--- src/component/TablaMakeorder.jsx
+++ src/component/TablaMakeorder.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import useTable from "../hooks/useTable.js";
 import TableFooter from "./TableFooter.jsx";
-import {Table, Checkbox, Button, Modal, Tooltip, Dropdown} from 'flowbite-react'
-import style from "../styles/Makeorder.module.css"
-import orderButton from "../component/orderButton.jsx"
+import {Table, Checkbox, Button, Modal } from 'flowbite-react';
+import style from "../styles/Makeorder.module.css";
+import myOrders from "../pages/myorders.jsx";
+import popup from "../component/popUp.jsx";
 
 const TablaMakeOrder = ({ data, rowsPerPage, searchValue, setSearchValue }) => {
     //componente que renderiza la tabla con los pedidos
@@ -22,67 +23,95 @@ const TablaMakeOrder = ({ data, rowsPerPage, searchValue, setSearchValue }) => {
 
     const handleCloseModal = () => {
         setShowModal(false);
-    }
+    };
 
     const handleOpenModal = () => {
         setShowModal(true);
+    };
+
+    const [ triggerPopUp, setTriggerPopUp ] = useState(false);
+
+    const handleClosePopUp = () => {
+        setTriggerPopUp(false)
     }
 
     return (
-        <>
-            <Table hoverable={true}>
-                {console.log(searchValue)}
-                <Table.Head>
-                    <Table.HeadCell className="" column=""></Table.HeadCell>
-                    <Table.HeadCell> Medicamento </Table.HeadCell>
-                    <Table.HeadCell> Activo/Excipiente </Table.HeadCell>
-                    <Table.HeadCell> PVP </Table.HeadCell>
-                    <Table.HeadCell> Dosis </Table.HeadCell>
-                    <Table.HeadCell> Detalles </Table.HeadCell>
-                    <Table.HeadCell><span className="sr-only"> Pedir </span></Table.HeadCell>
-                </Table.Head>
-                <Table.Body className="divide-y">
-                {slice.map((med) =>
-                    <>
-                        <Table.Row className={style.tableRow}>
-                            <Table.Cell className="!p-4"><Checkbox /></Table.Cell>
-                            <Table.Cell className={style.tableCell}>{med.name}</Table.Cell>
-                            <Table.Cell className={style.tableCell}>{med.act_exc}</Table.Cell>
-                            <Table.Cell className={style.tableCell}>{med.pvp}</Table.Cell>
-                            <Table.Cell className={style.tableCell}>{med.dosis}</Table.Cell>
-                            <Table.Cell className={style.tableCell}>{med.detalles}</Table.Cell>
-                            <Table.Cell>
-                                <React.Fragment>
-                                    <Button onClick={handleOpenModal}>
-                                        Pedir
-                                    </Button>
-                                    <Modal show={showModal} size="md" popup={true} onClose={handleCloseModal}>
-                                        <Modal.Header />
+        <div>
+            <>
+                <Table hoverable={true}>
+                    {console.log(searchValue)}
+                    <Table.Head>
+                        <Table.HeadCell className="" column=""></Table.HeadCell>
+                        <Table.HeadCell> Medicamento </Table.HeadCell>
+                        <Table.HeadCell> Activo/Excipiente </Table.HeadCell>
+                        <Table.HeadCell> PVP </Table.HeadCell>
+                        <Table.HeadCell> Dosis </Table.HeadCell>
+                        <Table.HeadCell> Detalles </Table.HeadCell>
+                        <Table.HeadCell><span className="sr-only"> Pedir </span></Table.HeadCell>
+                    </Table.Head>
+                    <Table.Body className="divide-y">
+                    {slice.map((med) =>
+                        <>
+                            <Table.Row className={style.tableRow}>
+                                <Table.Cell className="!p-4"><Checkbox /></Table.Cell>
+                                <Table.Cell className={style.tableCell}>{med.name}</Table.Cell>
+                                <Table.Cell className={style.tableCell}>{med.act_exc}</Table.Cell>
+                                <Table.Cell className={style.tableCell}>{med.pvp}</Table.Cell>
+                                <Table.Cell className={style.tableCell}>{med.dosis}</Table.Cell>
+                                <Table.Cell className={style.tableCell}>{med.detalles}</Table.Cell>
+                                <Table.Cell>
+                                    <>
+                                        <Button onClick={handleOpenModal}>
+                                            Pedir
+                                        </Button>
+                                        <Modal show={showModal} size="md" popup={true} onClose={handleCloseModal}>
+                                            <Modal.Header />
                                             <Modal.Body>
                                                 <div className="text-center">
                                                     <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                                        Seguro que quieres seguir con la compra ?
+                                                        Seguro que quieres comprar este medicamento ?
                                                     </h3>
-                                                        <div className="flex justify-center gap-4">
-                                                            <Button color="green" onClick={handleCloseModal}>
-                                                                Si, estoy segur@
-                                                            </Button>
-                                                            <Button color="failure" onClick={handleCloseModal}>
-                                                                No, cancelar
-                                                            </Button>
-                                                        </div>
+                                                    <div className="flex justify-center gap-4">
+                                                        <Button color="green" onClick={() => { setTriggerPopUp(true); setShowModal(false); }}>
+                                                            Si, estoy segur@
+                                                        </Button>
+                                                        <Button color="failure" onClick={handleCloseModal}>
+                                                            No, cancelar
+                                                        </Button>
+                                                    </div>
                                                 </div>
                                             </Modal.Body>
-                                    </Modal>
-                                </React.Fragment>                            
-                            </Table.Cell>
-                        </Table.Row>
-                    </>
-                )}
-                </Table.Body>
-            </Table>
+                                        </Modal>
+                                    </>                         
+                                </Table.Cell>
+                            </Table.Row>
+                        </>
+                    )}
+                    </Table.Body>
+                </Table>
+                <br></br>
+            </>
             <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
-        </>
+            { triggerPopUp && 
+                <Modal show={triggerPopUp} size="md" popup={true} onClose={handleClosePopUp} >
+                    <Modal.Header />
+                    <Modal.Body>
+                        <div className="text-center">
+                            <h3 className="mb-5 text-lg font-normal text-blue-500">
+                                Enhorabuena, tu pedido se ha realizado con éxito. Quieres ver su estado ?
+                            </h3>
+                            <div className="flex justify-center gap-4"> 
+                                <Button color="gray" href="./myorders" >
+                                    Ver Pedido
+                                </Button>
+                                <Button color="gray" onClick={handleClosePopUp} >
+                                    No, gracias.
+                                </Button>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal> }
+        </div>
     );
 }
 
