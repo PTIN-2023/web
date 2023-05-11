858b1f9cac9ef1f35c3e13c132619a441005d93b
AlejandroMayorC <alejandro.mayor@estudiantat.upc.edu>
2023-05-11 14:59:07 +0200

    API eliminar meds

--- src/pages/makeorder.jsx
+++ src/pages/makeorder.jsx
@@ -2,28 +2,86 @@ import Head from 'next/head'
 import Layout from "../component/Layout"
 import Tabla from "../component/TablaMakeorder"
 import myordersStyles from "../styles/Makeorder.module.css"
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import FilterTable from "../component/FilterTable"
 import { Row, Col } from 'react-bootstrap'
 
 import '../utils/med_server.js'
 
 export default function Home() {
-    const [searchValue, setSearchValue] = useState({value:"",isCompleted:false});
+    const [searchValue, setSearchValue] = useState({ value: "", isCompleted: false });
 
     let [meds, setMeds] = useState([])
 
-    useEffect(() => {
+    /*useEffect(() => {
         fetch("/api/meds")
             .then((res) => res.json())
             .then((json) => {
                 setMeds(json.meds)
             })
             .catch((err) => console.log(err))
-    }, [])
+    }, [])*/
+
+    const handleClick = () => {
+        const medicamentos = [
+            {
+                "medName": "Ibuprofeno",
+                "act_exc": "Ibuprofeno/Sorbitol (E-420)",
+                "pvp": "30 comprimidos 4,68$",
+                "dosis": "1 comprimido (600 mg) cada 6 a 8 horas",
+                "detalles": "Pedido con receta"
+            },
+            {
+                "medName": "Ibuprofeno",
+                "act_exc": "Ibuprofeno/Sorbitol (E-420)",
+                "pvp": "30 comprimidos 4,68$",
+                "dosis": "1 comprimido (600 mg) cada 6 a 8 horas",
+                "detalles": "Pedido sin receta"
+            },
+            {
+                "medName": "Ibuprofeno",
+                "act_exc": "Ibuprofeno/Sorbitol (E-420)",
+                "pvp": "30 comprimidos 4,68$",
+                "dosis": "1 comprimido (600 mg) cada 6 a 8 horas",
+                "detalles": "Pedido sin receta"
+            },
+            {
+                "medName": "Ibuprofeno",
+                "act_exc": "Ibuprofeno/Sorbitol (E-420)",
+                "pvp": "30 comprimidos 4,68$",
+                "dosis": "1 comprimido (600 mg) cada 6 a 8 horas",
+                "detalles": "Pedido sin receta"
+            }
+        ];
+
+        fetch('/api/makeorder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ medicamentos })
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+    };
+
+    const fetchMedicamentos = () => {
+        fetch('/api/medicamentos')
+            .then((res) => res.json())
+            .then((data) => console.log('Medicamentos:', data.medicamentos))
+            .catch((err) => console.error(err));
+    };
+
+    const handleButtonClick = () => {
+        fetchMedicamentos();
+    };
 
     return (
         <>
+            <button onClick={handleClick}>Enviar pedido</button>
+            <button onClick={handleButtonClick}>Consultar Medicamentos</button>
+
             <Head>
                 <title>TransMedWebPTIN</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -34,17 +92,17 @@ export default function Home() {
                 <Layout navBarValue={setSearchValue}>
 
                     <div className={myordersStyles.mainContainer}>
-                    {/**Tablamakeorder recibe cuantas filas va a renderizar, los datos y el valor para filtrar en caso d eque haya */}
-                    <Row>
-                        <Col xs={3}>
-                            <FilterTable />
-                        </Col>
-                        <Col xs={9}>
-                            <Tabla data={meds} rowsPerPage={10} searchValue={searchValue} setSearchValue={setSearchValue}/>
-                        </Col>
-                    </Row>
+                        {/**Tablamakeorder recibe cuantas filas va a renderizar, los datos y el valor para filtrar en caso d eque haya */}
+                        <Row>
+                            <Col xs={3}>
+                                <FilterTable />
+                            </Col>
+                            <Col xs={9}>
+                                <Tabla data={meds} rowsPerPage={10} searchValue={searchValue} setSearchValue={setSearchValue} />
+                            </Col>
+                        </Row>
                     </div>
-                </Layout> 
+                </Layout>
             </main>
         </>
     );
