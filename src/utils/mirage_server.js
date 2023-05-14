cd8a80175d979954a63c8f3982bed84dd5348a66
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2023-05-14 14:06:08 +0200

    readd deleted routes+seeds+models

--- src/utils/mirage_server.js
+++ src/utils/mirage_server.js
@@ -5,6 +5,9 @@ import { seedMirageDrones, defineMirageDroneRoutes } from "./mirage_calls/drones
 import { seedMirageAuth, defineMirageAuthRoutes } from "./mirage_calls/auth"
 import { seedMirageMapLocations, defineMirageMapLocationsRoutes } from "./mirage_calls/map_locations"
 import { seedMirageMisc, defineMirageMiscRoutes } from "./mirage_calls/misc"
+import { seedMirageMakeOrders, defineMakeOrdersRoutes } from "./mirage_calls/make_orders"
+import { seedMirageMyOrders, defineMirageMyOrdersRoutes } from "./mirage_calls/my_orders"
+import { seedMirageDoctorsNotifications, defineMirageDoctorsNotificationsRoutes } from "./mirage_calls/doctors_notifications"
 import { seedMirageUserInf, defineMirageProfileRoutes } from "./mirage_calls/profile"
 
 export function makeServer() {
@@ -14,16 +17,31 @@ export function makeServer() {
       users: Model,
       cars: Model,
       drones: Model,
-      medicamento: Model
+      medicines: Model,
+      prescriptions : Model,
+      orders : Model,
+      beehives : Model,
+      notifications : Model
     },
 
     seeds(server) {
-      seedMirageCars(server)
-      seedMirageDrones(server)
-      seedMirageAuth(server)
-      seedMirageMapLocations(server)
-      seedMirageMisc(server)
-      seedMirageUserInf(server) // Simula la creación de dos usuarios
+      if(typeof window !== 'undefined') {
+        const dbData = localStorage.getItem('db');
+        if (dbData) {
+          // https://miragejs.com/api/classes/db/#load-data
+          server.db.loadData(JSON.parse(dbData));
+        } else {
+          seedMirageCars(server)
+          seedMirageDrones(server)
+          seedMirageAuth(server)
+          seedMirageMapLocations(server)
+          seedMirageMisc(server)
+          seedMirageMakeOrders(server)
+          seedMirageMyOrders(server)
+          seedMirageDoctorsNotifications(server)
+          seedMirageUserInf(server) // Simula la creación de dos usuarios
+        }
+      }
     },
 
     routes() {
@@ -34,11 +52,15 @@ export function makeServer() {
       defineMirageAuthRoutes(this)
       defineMirageMapLocationsRoutes(this)
       defineMirageMiscRoutes(this)
+      defineMakeOrdersRoutes(this)
+      defineMirageMyOrdersRoutes(this)
+      defineMirageDoctorsNotificationsRoutes(this)
       defineMirageProfileRoutes(this) // Ruta para gestionar los datos de la pagina del PERFIL
 
       this.passthrough("https://api.mapbox.com/**")
       this.passthrough("https://events.mapbox.com/**")
       this.passthrough('https://www.googleapis.com/**');  
+      this.passthrough()
     }
   })
 }
