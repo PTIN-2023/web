38ae959d77892f6e0cf0f08b90d36ff6baa8846c
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2023-05-21 17:11:49 +0200

    add subitandfetch variant that returns the non-stringified object

--- src/hooks/useSumbitAndFetchObject.js
+++ src/hooks/useSumbitAndFetchObject.js
@@ -3,17 +3,17 @@ import { useState, useEffect } from "react";
 // Hook that returns a function to use in a sumbit form that automatically
 // sends a post request to the provided API endpoint with "request" as the body
 // Optionally, a function to evaluate the response can be provided. The given 
-// response is a string
+// response is an object
 //
 // Usage example:
 //   const [userToken, ] = useState('user_token')
 //   const request = usePrepareBodyRequest({
 //     "session_token" : userToken
 //   })
-//   [sumbitAndFetch, stringResponse] = useSumbitAndFetch(request, '/api/check', (res) => console.log(res))
+//   [sumbitAndFetch, response] = useSumbitAndFetch(request, '/api/check', (res) => console.log(res))
 
-export default function useSumbitAndFetch(request, url, evaluateResponse) {
-    const [stringResponse, setStringResponse] = useState('none');
+export default function useSumbitAndFetchObject(request, url, evaluateResponse) {
+    const [response, setResponse] = useState('none');
     
     async function fetchAndExtractBody(url, body) {
         return fetch(url, {
@@ -28,11 +28,11 @@ export default function useSumbitAndFetch(request, url, evaluateResponse) {
     const sumbitAndFetch = async (e) => {
         if(e) e.preventDefault();
         const res = await fetchAndExtractBody(url, request);
-        setStringResponse(JSON.stringify(res, null, 2))
+        setResponse(res)
 
         if(evaluateResponse)
             evaluateResponse(res)
     };
 
-    return [sumbitAndFetch, stringResponse];
+    return [sumbitAndFetch, response];
 };
