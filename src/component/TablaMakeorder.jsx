5e37d3d1ba2af8aff97f1ee8d2fd7249323f27c5
AlejandroMayorC <alejandro.mayor@estudiantat.upc.edu>
2023-05-23 11:51:31 +0200

    makeorders

--- src/component/TablaMakeorder.jsx
+++ src/component/TablaMakeorder.jsx
@@ -13,22 +13,23 @@ const TablaMakeOrder = ({ data, rowsPerPage, searchValue, setSearchValue }) => {
     const [page, setPage] = useState(1);
 
     //si la longitud del searchValue es > 0 y se hizo click en buscar, filtra el json de datos
-    if(searchValue.value.length > 0 && searchValue.isCompleted){
-        data = data.filter((med) => med.nombre.toLowerCase().includes(searchValue.value));  
-    }  
+    if (searchValue.value.length > 0 && searchValue.isCompleted) {
+        data = data.filter((med) => med.nombre.toLowerCase().includes(searchValue.value));
+    }
     var { slice, range } = useTable(data, page, rowsPerPage);
 
-    const {addToCart} = useContext(ShopContext);
+    const { addToCart } = useContext(ShopContext);
 
     return (
         <div className="mx-auto flex flex-col justify-start">
             <div className="bg-white">
                 <div className="mx-auto max-w-2x1 px-4 py-2 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Medicamentos</h2>
-                    
+                    <h2 className="text-2xl font-bold tracking-tight text-blue-800 sm:text-3xl md:text-4xl lg:text-5xl drop-shadow-xl font-serif">Medicamentos</h2>
+
+
                     <div className="mt-6 grid grid-cols-1 gap-x-6 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
                         {slice.map((med) => (
-                            <Product data={med}/>
+                            <Product data={med} />
                         ))}
                     </div>
                 </div>
@@ -37,7 +38,7 @@ const TablaMakeOrder = ({ data, rowsPerPage, searchValue, setSearchValue }) => {
                 <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
             </div>
         </div>
-        
+
     );
 }
 
