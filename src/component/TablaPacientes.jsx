0bb368502ffbbd10a854ef7aed0387ee797389a9
Aklix2606 <alexandre.picas@estudiantat.upc.edu>
2023-05-26 19:10:14 +0200

    Update Doctors Patients
    Implemented API call to get the data and fixed some data showing.

--- src/component/TablaPacientes.jsx
+++ src/component/TablaPacientes.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useRef } from "react";
 import useTable from "../hooks/useTable.js";
 import TableFooter from "./TableFooter.jsx";
-import {Table, Checkbox, Button, Modal, Tooltip, Dropdown} from 'flowbite-react'
+import {Table, Button, Modal, Dropdown} from 'flowbite-react'
 import myordersStyles from "../styles/Myorders.module.css"
-import {HiOutlineArrowRight, HiTrash, HiOutlineExclamationCircle} from "react-icons/hi"
+import { HiOutlineArrowRight } from "react-icons/hi"
+
 
 //TODO: modular estas funciones de modal
 function ModalContactar({currentTarget, currentItem, modalContactarState, setModalContactarState}){
@@ -40,9 +41,8 @@ function ModalContactar({currentTarget, currentItem, modalContactarState, setMod
             Sitio de contacto del cliente: {currentItem}
             </p>
             <p className="text-base font-bold leading-relaxed text-gray-500 dark:text-gray-400">
-            {/*TODO: en el proximo sprint, relacionar paciente con sus datos de doctor!*/}
-            +34 123456789 <br />
-            paciente.superbueno@gente.com
+              +34 123456789 <br />
+              paciente.superbueno@gente.com
             </p>
           </div>
         </Modal.Body>
@@ -57,39 +57,28 @@ function ModalContactar({currentTarget, currentItem, modalContactarState, setMod
 }
 
 
-const Tablapacientess = ({ data, rowsPerPage, searchValue, setSearchValue }) => {
+const Tablapacientess = ({ data, rowsPerPage }) => {
+
+
   //componente que renderiza la tabla con los pacientess
   //recibe data -> json de pacientess
   //rowsPerPage -> cuantas filas va a renderizar
-  //searchValue -> el filtro en caso de que se active el componente MyOrdersSearch
   const [page, setPage] = useState(1);
   //estos dos hooks de abajo sirven para mostrar o bien ocultar los modals
   const [modalContactarState, setModalContactarState] = useState(false);
-  const [modalCancelarpacientesState, setModalCancelarpacientesState] = useState(false);
   //currentTarget es un hook useRef para que no se actualice en cada render y así aseguramos que los modals no se multipliquen
   const currentTarget = useRef("");
-
-  //changeModalCancelarpacientesState y changeModalContactarState son funciones que cambian el useState ya que los setters no se pueden pasar bien hacia los componentes
-  function changeModalCancelarpacientesState(e){
-    setModalCancelarpacientesState(e);
-
-  }
 
   function changeModalContactarState(e){
     setModalContactarState(e);
 
   }
 
-  //si la longitud del searchValue es > 0 y se hizo click en buscar, filtra el json de datos
-  if(searchValue.value.length > 0 && searchValue.isCompleted){
-    data = data.filter((pacientes) => pacientes.nombre.toLowerCase().includes(searchValue.value));  
-  }  
   var { slice, range } = useTable(data, page, rowsPerPage);
   return (
     <>
         <TableFooter range={range} slice={slice} setPage={setPage} page={page} />
         <Table hoverable={true}>
-          {console.log(searchValue)}
           <Table.Head>
             <Table.HeadCell className="!p-4">
             </Table.HeadCell>
@@ -116,20 +105,18 @@ const Tablapacientess = ({ data, rowsPerPage, searchValue, setSearchValue }) => 
                     {pacientes.nombre}
                   </Table.Cell>
                   <Table.Cell> 
-                    <Dropdown className={myordersStyles.chevronDown} label={pacientes.historial[0]} inline={true}>
-                        <option>{pacientes.historial[1]}</option>
-                        <option>{pacientes.historial[2]}</option>
-                        {/**TODO: Hacer bucle para mostrar el historial*/}
-                    </Dropdown>
+                    <Dropdown label="Alergias/Enfermedades" inline={true}>
+                        {pacientes.historial.map(med =>
+                            <Dropdown.Item>{med}</Dropdown.Item>
+                        )}
+                    </Dropdown>   
                   </Table.Cell>
-                  <Table.Cell>
-                    <Dropdown className={myordersStyles.chevronDown} label={pacientes.medicamentos[0]} inline={true}>
-                        <option>{pacientes.medicamentos[1]}</option>
-                        <option>{pacientes.medicamentos[2]}</option>
-                        <option>{pacientes.medicamentos[3]}</option>
-                        <option>{pacientes.medicamentos[4]}</option>
-                        {/**TODO: Hacer bucle para mostrar los medicamentos ya que no siempre habra 4*/}
-                    </Dropdown>                                                  
+                  <Table.Cell>    
+                    <Dropdown label="Lista Medicamentos" inline={true}>
+                        {pacientes.medicamentos.map(med =>
+                            <Dropdown.Item>{med}</Dropdown.Item>
+                        )}
+                    </Dropdown>                                              
                   </Table.Cell>
                   <Table.Cell> 
                     <ModalContactar currentTarget={currentTarget} currentItem={pacientes.nombre} modalContactarState={modalContactarState} setModalContactarState={changeModalContactarState}/>
@@ -137,10 +124,8 @@ const Tablapacientess = ({ data, rowsPerPage, searchValue, setSearchValue }) => 
                 </Table.Row>
             </>
           )}
-
           </Table.Body>
         </Table> 
-
     </>
   );
 };
