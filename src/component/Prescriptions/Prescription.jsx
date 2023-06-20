ec587fac48f169e2b49f7661e90995a84a4261a6
Aklix2606 <94629313+Aklix2606@users.noreply.github.com>
2023-06-20 16:34:04 +0200

    Add patient and medicine list
    Add a container for patient and medicine tables

--- src/component/Prescriptions/Prescription.jsx
+++ src/component/Prescriptions/Prescription.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useRef } from 'react';
-import { Button, Modal } from 'flowbite-react';
+import { Button, Modal, Tabs } from 'flowbite-react';
 import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 import download from 'downloadjs';
 import styles from '../../styles/Prescriptions.module.css';
-import { HiUserGroup, HiPaperAirplane, HiDownload } from "react-icons/hi";
-import logo from '../../../public/media/logo/Blanco.png'
+import { HiUserGroup, HiPaperAirplane, HiDownload, HiOutlineBell, HiClock } from "react-icons/hi";
+import { BsCapsulePill } from "react-icons/bs";
+
+import Tablapacientess from "../TablaPacientes";
 
 function ListPatients () {
 
@@ -62,19 +64,6 @@ export default function MakePrescriptions() {
         const page = pdfDoc.addPage()
         const { width, height } = page.getSize()
         const fontSize = 30
-
-        /*
-        const logoUrl = process.env.PUBLIC_URL + '/media/logo/Blanco.png';
-        const logoImageBytes = await fetch(logoUrl).then(res => res.arrayBuffer());
-        const logoImage = await pdfDoc.embedPng(logoImageBytes)
-
-        const form = pdfDoc.getForm()
-        const button = form.createButton('logo.button')
-        button.addToPage('logo', page, { x: 50, y: 900, size: 15 })
-
-        const logoImageField = form.getButton('logo.button')
-        logoImageField.setImage(logoImage)
-        */
 
         page.drawText('TransMed', { x: 50, y: 800, size: 40, color:  rgb(0.176, 0.165, 0.439)})
 
@@ -146,28 +135,35 @@ export default function MakePrescriptions() {
             <p className={styles['titulo']}>Nueva Receta</p>
             <form onSubmit={handleSubmitGenerate}>
                 <div className={styles['input-group']}>
+
+                    <label htmlFor="patientName" className={styles.label}>Nombre Paciente:</label>
+
                     <div className={styles['input-container']}>
-                        <label htmlFor="patientName" className={styles.label}>Nombre Paciente:</label>
                         <input type="text" required id="patientName" name="patientName" className={styles.inputNombre} onChange={handleNombreInput}/>
                         <ListPatients/>
                     </div>
+
+                    <label htmlFor="medicationName" className={styles.label}>Nombre Medicamento:</label>
                 
                     <div className={styles['input-container']}>
-                        <label htmlFor="medicationName" className={styles.label}>Nombre Medicamento:</label>
                         <input type="text" required id="medicationName" name="medicationName" className={styles.inputMedicamento} onChange={handleMedicamentoInput}/>
                         <ListMedicines/>
                     </div>
-                
+
+                    <label htmlFor="treatmentDuration" className={styles.label}>Duración tratamiento:</label>
+
                     <div className={styles['input-container']}>
-                        <label htmlFor="treatmentDuration" className={styles.label}>Duración tratamiento:</label>
                         <input type="text" required id="treatmentDuration" name="treatmentDuration" className={styles.inputTratamiento} onChange={handleTratamientoInput}/>
                     </div>
-                </div>
+                
 
-                <div className={styles['textarea-group']}>
                     <label htmlFor="notes" className={styles.label}>Notas:</label>
-                    <div className={styles['input-container']}>
-                        <textarea id="notes" name="notes" className={styles.textarea} />
+
+                    <div className={styles['textarea-group']}>
+                        
+                        <div className={styles['input-container']}>
+                            <textarea id="notes" name="notes" className={styles.textarea} />
+                        </div>
                     </div>
                 </div>
 
@@ -204,6 +200,25 @@ export default function MakePrescriptions() {
                     </Modal.Body>
                 </Modal>
             </form>
+            <div className={styles['tabla']}>
+                <Tabs.Group
+                    aria-label="pills"
+                    style="underline"
+                >
+                    <Tabs.Item 
+                        active={true}
+                        title="Pacientes"
+                        icon={HiUserGroup}
+                    >
+                    </Tabs.Item>
+                    <Tabs.Item
+                        title="Medicamentos"
+                        icon={BsCapsulePill}
+                    >
+                    </Tabs.Item>
+                </Tabs.Group>
+            </div>
+            
         </div>
         
         
