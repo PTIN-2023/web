5d1e5d4d5beeb7a52443c3b47f03a66742f9569c
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-06-23 17:07:11 +0200

    Update orders.jsx

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -69,7 +69,7 @@ export default function Home(props) {
         <Layout navBarValue={setSearchValue}>
         <div className={myordersStyles.mainContainer}>
           {/**TablaPedidos recibe cuantas filas va a renderizar, los datos y el valor para filtrar en caso d eque haya */}
-          {("Aquest pacient no t\u00e9 cap ordre" in stringResponse || "No tienes token para poder comprobar esto, espabila" in stringResponse) && <TablaPedidosManager data={JSON.parse(stringResponse)} rowsPerPage={10} searchValue={searchValue} setSearchValue={setSearchValue}/>}
+          {(stringResponse != "none") && ("Aquest pacient no t\u00e9 cap ordre" in stringResponse || "No tienes token para poder comprobar esto, espabila" in stringResponse) && <TablaPedidosManager data={JSON.parse(stringResponse)} rowsPerPage={10} searchValue={searchValue} setSearchValue={setSearchValue}/>}
           
         </div>
         </Layout> 
