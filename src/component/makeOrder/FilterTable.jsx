f2382c3caf83dd6f43923467ed846a10aab43a2c
RabadanDotDev <77171479+RabadanDotDev@users.noreply.github.com>
2023-07-01 23:49:36 +0200

    fix make order filter

--- src/component/makeOrder/FilterTable.jsx
+++ src/component/makeOrder/FilterTable.jsx
@@ -2,85 +2,43 @@ import React, { useState } from "react";
 import rangePriceStyles from "../../styles/price.module.css"
 import { Sidebar } from "flowbite-react";
 
-const FilterTable = ({medName, pvpMin, pvpMax, prescriptionNeeded, medForm, typeOfAdminst, setMedName, setPvpMin, setPvpMax, setPrescriptionNeeded, setMedForm, setTypeOfAdminst}) => {
+const CheckBoxComponent = ({list, element, setter}) => {
+    const addToList = () => {
+        if (list.indexOf(element) === -1) {
+            setter(list.concat(element))
+        }
+    }    
     
-    let arrayMedForm = medForm
+    const removeFromList = () => {
+        if (list.indexOf(element) !== -1) {
+            setter(list.filter(item => item != element))
+        }
+    }
 
+    return(<Sidebar.Item style={{ marginBottom: '-15px', marginTop: '-10px' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <input 
+                type="checkbox" 
+                className="" 
+                checked={list.includes(element)} 
+                onChange={() => list.includes(element) ? removeFromList() : addToList()}
+            />
+            &nbsp;&nbsp;&nbsp;
+            <label>
+                {element}
+            </label>
+        </div>
+    </Sidebar.Item>)
+}
+
+const FilterTable = ({pvpMax, medForm, typeOfAdminst, setPvpMax, setMedForm, setTypeOfAdminst}) => {
     // Defered change of pvp value
-    const [price, setPrice] = useState(50);
-    const [isCheckedConReceta, setIsCheckedConReceta] = useState(true)
-    const [isPill, setIsPill] = useState(true)
-    const [isPowder, setIsPowder] = useState(true)
-    const [isLiquid, setIsLiquid] = useState(true)
-    
-    //oral, topic
-    
+    const [priceTmp, setPriceTmp] = useState(pvpMax);
+
     const changePrice = (event) => {
-        setPrice(event.target.value);
+        setPriceTmp(event.target.value);
     }
-
-    const submitPriceChange = (_) => {
-        setPvpMax(price)
-    }
-
-    const conReceta = (_) => {
-        if (isCheckedConReceta === true) setPrescriptionNeeded(false)
-        else setPrescriptionNeeded(true)
-        setIsCheckedConReceta(!isCheckedConReceta)
-    }
-
-    const setPastilla = (_) => {
-        let auxMedArray = []
-        for (let i in medForm) {
-            if(medForm[i] != "pill" && medForm[i] != "cream") {
-                auxMedArray.push(medForm[i])
-            }
-        }
-        medForm = auxMedArray
-        console.log(medForm)
-        if (isPill === true) setMedForm(medForm)
-        else {
-            medForm.push("pill")
-            medForm.push("cream")
-            setMedForm(medForm)
-        }
-        setIsPill(!isPill)
-    }
-
-    const setPolvo = (_) => {
-        let auxMedArray = []
-        for (let i in medForm) {
-            if(medForm[i] != "powder") {
-                auxMedArray.push(medForm[i])
-            }
-        }
-        medForm = auxMedArray
-        console.log(medForm)
-        if (isPowder === true) setMedForm(medForm)
-        else {
-            medForm.push("powder")
-            setMedForm(medForm)
-        }
-        setIsPowder(!isPowder)
-    }
-
-    const setLiquid = (_) => {
-        let auxMedArray = []
-        for (let i in medForm) {
-            if(medForm[i] != "liquid") {
-                auxMedArray.push(medForm[i])
-            }
-        }
-        medForm = auxMedArray
-        console.log(medForm)
-        if (isLiquid === true) setMedForm(medForm)
-        else {
-            medForm.push("liquid")
-            setMedForm(medForm)
-        }
-        setIsLiquid(!isLiquid)
-    }
-
+    
     return(
         <span>
             <div className="mx-auto">
@@ -89,66 +47,29 @@ const FilterTable = ({medName, pvpMin, pvpMax, prescriptionNeeded, medForm, type
                         <Sidebar.ItemGroup>
                             <label style={{ fontWeight: 'bold' }}>PRECIO</label>
                             <Sidebar.Item>
-                                <h4>{price}€</h4>
-                                <input type="range" onChange={changePrice} onMouseUp={submitPriceChange} min={0} max={100} step={1} value={price}/>
+                                <h4>{priceTmp}€</h4>
+                                <input type="range" onChange={changePrice} onMouseUp={() => {setPvpMax(priceTmp)}} min={0} max={100} step={1} value={priceTmp}/>
                             </Sidebar.Item>
                         </Sidebar.ItemGroup>
                         <Sidebar.ItemGroup>
-                            <label style={{ fontWeight: 'bold' }}>RECETA</label>
-                            <Sidebar.Item style={{ marginBottom: '-15px', marginTop: '-10px' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked={isCheckedConReceta} onClick={conReceta} />&nbsp;&nbsp;&nbsp;
-                                    <label >Con receta</label>
-                                </div>
-                            </Sidebar.Item>
-                        </Sidebar.ItemGroup>
-                        <Sidebar.ItemGroup>
-                            <label style={{ fontWeight: 'bold' }}>FORMA</label>
-                            <Sidebar.Item style={{ marginBottom: '-15px', marginTop: '-10px' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked={isPill} onClick={setPastilla} />&nbsp;&nbsp;&nbsp;
-                                    <label>Pastillas / Cremas</label>
-                                </div>
-                            </Sidebar.Item>
-                            <Sidebar.Item style={{ marginBottom: '-15px' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked={isPowder} onClick={setPolvo} />&nbsp;&nbsp;&nbsp;
-                                    <label >Polvos</label>
-                                </div>
-                            </Sidebar.Item>
-                            <Sidebar.Item>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked={isLiquid} onClick={setLiquid} />&nbsp;&nbsp;&nbsp;
-                                    <label >Líquido</label>
-                                </div>
-                            </Sidebar.Item>
+                            <label style={{ fontWeight: 'bold' }}>FORM</label>
+                            {["Tablets", "Capsules", "Liquid", "Powder", "Cream", "Gel"].map((v) => 
+                                <CheckBoxComponent
+                                    list={medForm}
+                                    element={v}
+                                    setter={setMedForm}
+                                />
+                            )}
                         </Sidebar.ItemGroup>
                         <Sidebar.ItemGroup>
                             <label style={{ fontWeight: 'bold' }}>VÍA</label>
-                            <Sidebar.Item style={{ marginBottom: '-15px', marginTop: '-10px'}}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked/>&nbsp;&nbsp;&nbsp;
-                                    <label >Oral</label>
-                                </div>
-                            </Sidebar.Item>
-                            <Sidebar.Item style={{ marginBottom: '-15px' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked/>&nbsp;&nbsp;&nbsp;
-                                    <label >Tópico</label>
-                                </div>
-                            </Sidebar.Item>
-                            <Sidebar.Item style={{ marginBottom: '-15px' }}>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked/>&nbsp;&nbsp;&nbsp;
-                                    <label >Inhalatoria</label>
-                                </div>
-                            </Sidebar.Item>
-                            <Sidebar.Item>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <input type="checkbox" className="" checked/>&nbsp;&nbsp;&nbsp;
-                                    <label >Oftalmológica</label>
-                                </div>
-                            </Sidebar.Item>
+                            {["Oral","Topical","Inhalation","Ophthalmic"].map((v) => 
+                                <CheckBoxComponent
+                                    list={typeOfAdminst}
+                                    element={v}
+                                    setter={setTypeOfAdminst}
+                                />
+                            )}
                         </Sidebar.ItemGroup>
                     </Sidebar.Items>
                 </Sidebar>
--- src/pages/makeorder.jsx
+++ src/pages/makeorder.jsx
@@ -1,75 +1,36 @@
 import Head from 'next/head'
 import Layout from "../component/Layout"
 import Tabla from "../component/makeOrder/TablaMakeorder"
-import makeorderStyles from "../styles/Makeorder.module.css"
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import FilterTable from "../component/makeOrder/FilterTable"
 import { ShopContextProvider } from '../context/shopContext'
-import * as env_config from "../utils/env_config.js";
 import useCookie from "../hooks/useCookie"
-import usePrepareBodyRequest from "../hooks/usePrepareBodyRequest.js";
 import useSumbitAndFetchObject from "../hooks/useSumbitAndFetchObject.js";
 import commonGetServerSideProps from '../utils/gen_common_props';
 import ShoppingCart from "../component/makeOrder/shoppingCart"
+import usePrepareBodyRequest from '../hooks/usePrepareBodyRequest';
 
 export async function getServerSideProps() {
   return await commonGetServerSideProps()
 }
 
-
-// esta funcion filtra los medicamentos que no tienen prescripción
-function deletePrescription (meds) {
-    const arrayMeds = []
-    for (let i in meds) {
-        if(meds[i].prescription_needed === false) {
-            arrayMeds.push(meds[i])
-        }
-    }
-    return arrayMeds
-}
-
-
-
-// esta función filtra los medicamentos que són pill/cream
-function formFilter (meds, medsForms) {
-    // medForm és un array que contiene la forma de los medicamentos que queremos que aparezcan
-    // esta función revisa este array con los medicamentos que nos llegan i los filtra
-    let auxArray = []
-    for (let i in meds) {
-        let med = meds[i] //single med
-        console.log("type_admin", med.type_of_administration)
-        for (let y in medsForms) {
-            if (med.form === medsForms[y]) auxArray.push(med) // si la forma és la correcta afegim el medicament al array
-        }
-    }
-    meds = auxArray
-    return meds
-}
-
 export default function Home(props) {
-    const [searchValue, setSearchValue] = useState({value:"",isCompleted:false});
-
     // Cookies
     const [userTokenCookie, ] = useCookie('user_token')
 
     // Form values
-    const [medName, setMedName] = useState();
-    const [pvpMin, setPvpMin] = useState(0);
+    const [medName, setMedName] = useState({value:"",isCompleted:false});
     const [pvpMax, setPvpMax] = useState(50);
-    const [prescriptionNeeded, setPrescriptionNeeded] = useState(null)
-    const [medForm, setMedForm] = useState(["pill", "cream", "powder", "liquid"])
-    const [typeOfAdminst, setTypeOfAdminst] = useState(["oral","topical","inhalation","ophthalmic"])
+    const [medForm, setMedForm] = useState(["Tablets", "Capsules", "Liquid", "Powder", "Cream", "Gel"])
+    const [typeOfAdminst, setTypeOfAdminst] = useState(["Oral","Topical","Inhalation","Ophthalmic"])
     const [page, setPage] = useState(1);
-    const [medsPerPage, setMedsPerPage] = useState(10);
+    const [medsPerPage, ] = useState(10);
 
-    // Request
     const stringRequest = usePrepareBodyRequest({
         "session_token" : userTokenCookie,
         "filter": {
             "med_name": medName,
-            "pvp_min": pvpMin,
             "pvp_max": pvpMax,
-            "prescription_needed": prescriptionNeeded,
             "form": medForm,
             "type_of_administration": typeOfAdminst,
             "page": page,
@@ -77,21 +38,15 @@ export default function Home(props) {
         }
     })
     
-    const [sumbitAndFetch, response] = useSumbitAndFetchObject(
+    const [sumbitListAvailable, response] = useSumbitAndFetchObject(
         stringRequest,
-        props.apiEndpoint+"/api/list_available_medicines",
+        props.apiEndpoint + "/api/list_available_medicines",
     )
 
     useEffect(() => {
         if(userTokenCookie != null)
-            sumbitAndFetch();
-    }, [page, stringRequest])
-
-    let medsArray = response.medicines
-    // filtro con/sin receta
-    if (prescriptionNeeded === false) medsArray = deletePrescription(response.medicines)
-    // filtro form
-    //medsArray = formFilter(medsArray, medForm)
+            sumbitListAvailable()
+    }, [stringRequest])
     
     return (
         <>
@@ -102,37 +57,28 @@ export default function Home(props) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                {/**le pasamos a Layout el valor del componente de la página que está renderizando (Layout se encarga de detectar en que pagina está) */}
-                <Layout navBarValue={setSearchValue} props={props}>
-                    <div className='flex space-between flex-start' style={{ backgroundColor: '#87CEFA' }}>
-                        <div>
-                        <ShoppingCart />
-                        {/*console.log(medForm)*/}
-                            <FilterTable 
-                                medName={medName}
-                                pvpMin={pvpMin}
-                                pvpMax={pvpMax}
-                                prescriptionNeeded={prescriptionNeeded}
-                                medForm={medForm}
-                                typeOfAdminst={typeOfAdminst}
+            <Layout navBarValue={setMedName} props={props}>
+                <div className='flex space-between flex-start' style={{ backgroundColor: '#87CEFA' }}>
+                    <div>
+                        <FilterTable 
+                            pvpMax={pvpMax}
+                            medForm={medForm}
+                            typeOfAdminst={typeOfAdminst}
 
-                                setMedName={setMedName}
-                                setPvpMin={setPvpMin}
-                                setPvpMax={setPvpMax}
-                                setPrescriptionNeeded={setPrescriptionNeeded}
-                                setMedForm={setMedForm}
-                                setTypeOfAdminst={setTypeOfAdminst}
-                            />
-                        </div>
-                        
-                        <div style={{ flex: 1, marginLeft: '5px' }}>
-                            {/**Tablamakeorder recibe cuantas filas va a renderizar, los datos y el valor para filtrar en caso d eque haya */}                 
-                            <Tabla medicineResponse={medsArray} page={page} setPage={setPage} />
-                        </div>
+                            setPvpMax={setPvpMax}
+                            setMedForm={setMedForm}
+                            setTypeOfAdminst={setTypeOfAdminst}
+                        />
                     </div>
-                </Layout> 
+                    <div style={{ flex: 1, marginLeft: '5px' }}>              
+                        <Tabla medicineResponse={response.medicines} page={page} setPage={setPage} />
+                    </div>
+                </div>
+            </Layout>
             </main>
             </ShopContextProvider>
         </>
     );
 }
+
+/* */
