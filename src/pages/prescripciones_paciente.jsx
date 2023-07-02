82266534301c8f8c341d2f87daf87e46dff70258
Marc <marc.perez.guerrero@estudiantat.upc.edu>
2023-07-02 19:46:32 +0200

    PrescripcionesPaciente Update

--- src/pages/prescripciones_paciente.jsx
+++ src/pages/prescripciones_paciente.jsx
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Layout from "../component/Layout"
 import useCookie from "../hooks/useCookie";
 import useAutoSumbitAndFetchObject from "../hooks/useAutoSumbitAndFetchObject";
+import { HiDownload } from "react-icons/hi"
 import commonGetServerSideProps from '../utils/gen_common_props';
 import getTextCurrentLocale from '../utils/getTextCurrentLocale';
+import createPDF from '../utils/createPDF';
+import download from 'downloadjs';
 import inventoryStyles from "../styles/Inventory.module.css"
 import { Table } from 'flowbite-react'
 
@@ -11,14 +14,16 @@ export async function getServerSideProps() {
   return await commonGetServerSideProps()
 }
 
+
+
 const CustomTableRow = ({ entry, props }) => {
   const [userTokenCookie,] = useCookie('user_token')
 
   const [_, response] = useAutoSumbitAndFetchObject(
     // request values
     {
-      "session_token" : userTokenCookie,
-      "prescription_identifier" : entry.prescription_identifier
+      "session_token": userTokenCookie,
+      "prescription_identifier": entry.prescription_identifier
     },
     // url
     props.apiEndpoint + "/api/get_meds_prescription",
@@ -28,37 +33,49 @@ const CustomTableRow = ({ entry, props }) => {
     }
   )
 
+  const handleDownloadRecipie = (recipie) => {
+    createPDF("NombrePaciente", recipie.medicine_list, recipie.duration, recipie.notes, recipie.renewal, recipie.prescription_identifier, props).then((pdfBytes) => {
+      download(pdfBytes, "Receta.pdf", "application/pdf");
+    });
+  }
+
   return (
-  <Table.Row className={inventoryStyles.tableRow}>
-    <Table.Cell className={inventoryStyles.tableCell}>{entry.prescription_identifier}</Table.Cell>
-    <Table.Cell className={inventoryStyles.tableCell}>
-      <ul>
-      {response != "none" && response.medicine_list && response.medicine_list.map((med) => 
-        <li>{med.medicine_name} ({med.quantitat})</li>
-      )}
-      </ul>
-    </Table.Cell>
-    <Table.Cell className={inventoryStyles.tableCell}>{entry.duration}</Table.Cell>
-    <Table.Cell className={inventoryStyles.tableCell}>{entry.renewal}</Table.Cell>
-    <Table.Cell className={inventoryStyles.tableCell}>{entry.last_used}</Table.Cell>
-    <Table.Cell className={inventoryStyles.tableCell}>{entry.notes}</Table.Cell>
-  </Table.Row>
+    <Table.Row className={inventoryStyles.tableRow}>
+      <Table.Cell className={inventoryStyles.tableCell}>{entry.prescription_identifier}</Table.Cell>
+      <Table.Cell className={inventoryStyles.tableCell}>
+        <ul>
+          {response != "none" && response.medicine_list && response.medicine_list.map((med) =>
+            <li>{med.medicine_name} ({med.quantitat})</li>
+          )}
+        </ul>
+      </Table.Cell>
+      <Table.Cell className={inventoryStyles.tableCell}>{entry.duration}</Table.Cell>
+      <Table.Cell className={inventoryStyles.tableCell}>{entry.renewal}</Table.Cell>
+      <Table.Cell className={inventoryStyles.tableCell}>{entry.last_used}</Table.Cell>
+      <Table.Cell className={inventoryStyles.tableCell}>
+        <HiDownload
+          size={20}
+          style={{ cursor: 'pointer' }}
+          onClick={handleDownloadRecipie(entry)}
+        />
+      </Table.Cell>
+    </Table.Row>
   )
 }
 
-const CustomTable = ({ data, props }) => { 
+const CustomTable = ({ data, props }) => {
   return (
     <>
-        <Table hoverable={true}>
-          <Table.Head>
-            <Table.HeadCell> {getTextCurrentLocale('identifier')} </Table.HeadCell>
-            <Table.HeadCell> {getTextCurrentLocale('medicines')} </Table.HeadCell>
-            <Table.HeadCell> {getTextCurrentLocale('duration')} </Table.HeadCell>
-            <Table.HeadCell> {getTextCurrentLocale('renewal')} </Table.HeadCell>
-            <Table.HeadCell> {getTextCurrentLocale('last_used')} </Table.HeadCell>
-            <Table.HeadCell> {getTextCurrentLocale('notes') /*{getTextCurrentLocale('check_recipie')}*/} </Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="divide-y">
+      <Table hoverable={true}>
+        <Table.Head>
+          <Table.HeadCell> {getTextCurrentLocale('identifier')} </Table.HeadCell>
+          <Table.HeadCell> {getTextCurrentLocale('medicines')} </Table.HeadCell>
+          <Table.HeadCell> {getTextCurrentLocale('duration')} </Table.HeadCell>
+          <Table.HeadCell> {getTextCurrentLocale('renewal')} </Table.HeadCell>
+          <Table.HeadCell> {getTextCurrentLocale('last_used')} </Table.HeadCell>
+          <Table.HeadCell> {getTextCurrentLocale('check_recipie')} </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
           {data.map((entry) =>
             <CustomTableRow
               key={entry.prescription_identifier}
@@ -66,8 +83,8 @@ const CustomTable = ({ data, props }) => {
               props={props}
             />
           )}
-          </Table.Body>
-        </Table>
+        </Table.Body>
+      </Table>
     </>
   );
 };
@@ -78,7 +95,7 @@ export default function Home(props) {
   const [_, response] = useAutoSumbitAndFetchObject(
     // request values
     {
-      "session_token" : userTokenCookie,
+      "session_token": userTokenCookie,
     },
     // url
     props.apiEndpoint + "/api/get_patient_prescription_history",
@@ -91,14 +108,14 @@ export default function Home(props) {
   // Html
   return (<>
     <Head>
-        <title>TransMedWebPTIN</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+      <title>TransMedWebPTIN</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
     <main>
       <Layout props={props}>
         {response != 'none' && response.result == 'ok' &&
-          <CustomTable 
+          <CustomTable
             data={response.prescriptions}
             props={props}
           />
