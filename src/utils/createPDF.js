4fc25fd50a10e054e320fe8788d39017d2fea4ba
Aklix2606 <94629313+Aklix2606@users.noreply.github.com>
2023-07-03 20:51:07 +0200

    Add locale text to createPDF

--- src/utils/createPDF.js
+++ src/utils/createPDF.js
@@ -1,10 +1,16 @@
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import { svgObject } from "qr-image-color";
 
+import { getText } from "../utils/getTextCurrentLocale.js";
+import useCookie from '../hooks/useCookie';
+
 export default async function generatePDF(nombrePaciente, nombreMedicamento, tratamiento, notas, renwal, codigo, props) {
+
+  const [localeCookie, ] = useCookie('local');
+
   const doc = await PDFDocument.create();
   const page = doc.addPage();
-
+  /*
   const logoUrl = props.apiEndpoint + '/_next/image?url=%2Fmedia%2Flogo%2FBlanco-layout.png&w=256&q=75'
   const logoResponse = await fetch(logoUrl);
   console.log(logoResponse);
@@ -18,7 +24,7 @@ export default async function generatePDF(nombrePaciente, nombreMedicamento, tra
     width: logoDims.width/2,
     height: logoDims.height/2,
   })
-  
+  */
 
   const font = await doc.embedFont(StandardFonts.Helvetica);
 
@@ -33,18 +39,17 @@ export default async function generatePDF(nombrePaciente, nombreMedicamento, tra
     scale: desiredSize / qr.size
   });
 
-  //page.drawText('TransMed', { x: 50, y: 800, size: 40, color: rgb(0.176, 0.165, 0.439), font });
-  page.drawText('Codigo:', { x: 350, y: 800, size: 20, font });
-  page.drawText(`Nombre Paciente: ${nombrePaciente}`, { x: 50, y: 650, size: 15, font });
-  page.drawText(`Medicamentos:`, { x: 50, y: 620, size: 15, font });
+  page.drawText(getText("pdf_code", localeCookie), { x: 350, y: 800, size: 20, font });
+  page.drawText(getText("user_full_name", localeCookie) + `: ${nombrePaciente}`, { x: 50, y: 650, size: 15, font });
+  page.drawText(getText("medicines", localeCookie) + `:`, { x: 50, y: 620, size: 15, font });
   nombreMedicamento.forEach((medicamento, index) => {
     const { idMedicamento, cantidad, medicineName } = medicamento;
     const yPosition = 600 - (index * 20);
-    page.drawText(`Nombre: ${medicineName} - Id: ${idMedicamento} - Cantidad: ${cantidad}`, { x: 50, y: yPosition, size: 15, font });
+    page.drawText(getText("name", localeCookie)`: ${medicineName} - ` + getText("ID", localeCookie) + `: ${idMedicamento} - ` + getText("quantity", localeCookie) + `: ${cantidad}`, { x: 50, y: yPosition, size: 15, font });
   });
-  page.drawText(`Renewal: ${renwal}`, { x: 50, y: 590 - (nombreMedicamento.length * 20), size: 15, font });
-  page.drawText(`Duracion: ${tratamiento}`, { x: 50, y: 560 - (nombreMedicamento.length * 20), size: 15, font });
-  page.drawText(`Notas:`, { x: 50, y: 530 - (nombreMedicamento.length * 20), size: 15, font });
+  page.drawText(getText("renewal", localeCookie) + `: ${renwal}`, { x: 50, y: 590 - (nombreMedicamento.length * 20), size: 15, font });
+  page.drawText(getText("duration", localeCookie) + `: ${tratamiento}`, { x: 50, y: 560 - (nombreMedicamento.length * 20), size: 15, font });
+  page.drawText(getText("notes", localeCookie) + `:`, { x: 50, y: 530 - (nombreMedicamento.length * 20), size: 15, font });
   
   const form = doc.getForm();
   const notasField = form.createTextField('notas');
@@ -61,7 +66,6 @@ export default async function generatePDF(nombrePaciente, nombreMedicamento, tra
   });
   notasField.setFontSize(15);
 
-  
   
   const pdfBytes = await doc.save();
   return pdfBytes;
